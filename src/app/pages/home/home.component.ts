489344248f3e162eef7de9a3eeea258e8db5ee1c
Saeid Babaei <babaei503@gmail.com>
2019-08-21 16:11:16 +0430

    Add slide show to home page.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,9 +9,17 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  imageSources : string[]; 
+  constructor(private router: Router) {
+    this.imageSources = new Array("../../../assets/images/Tehran-Iran.jpg",
+                                  "../../../assets/images/tehran1.jpg",
+                                  "../../../assets/images/esfahan.jpg",
+                                  "../../../assets/images/esfahan1.jpg",
+                                  "../../../assets/images/esfahan2.jpg",
+                                  "../../../assets/images/Shiraz.jpg")
+   }
 
   ngOnInit() {
-
+    
   }
 }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 
 import { ThemeModule } from '../../@theme/theme.module';
 import { HomeRoutingModule, routedComponents } from './home-routing.module';
+import {SlideshowModule} from 'ng-simple-slideshow';
 
 
 @NgModule({
   imports: [
     ThemeModule,
     HomeRoutingModule,
+    SlideshowModule,
   ],
   declarations: [
     ...routedComponents,
